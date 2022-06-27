e1665683e63c1118e1679967f34788343a68eab8
kyahn23 <kyahn23@gmail.com>
2022-06-27 23:58:48 +0900

    props.children

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,16 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
   return (
-    <div className="expense-item">
-      {/* <div>
-        <div>{year}</div>
-        <div>{month}</div>
-        <div>{day}</div>
-      </div> */}
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
-}
+};
 export default ExpenseItem;
---
+++ src/components/UI/Card.js
@@ -0,0 +1,8 @@
+import "./Card.css";
+
+const Card = (props) => {
+  const classes = "card " + props.className;
+  return <div className={classes}>{props.children}</div>; // .children =>예약어
+};
+
+export default Card;
