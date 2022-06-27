503b85ee2afa162920c1acad9a49bab663aaa2cc
kyahn23 <kyahn23@gmail.com>
2022-06-28 01:36:01 +0900

    컴포넌트 통신

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState } from "react/cjs/react.production.min";
 import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
 
 const App = () => {
   const expenses = [
@@ -23,9 +25,15 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expenses);
+  };
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
